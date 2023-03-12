d9129e5e41634620125a5694a18a9645f17ec5d8
waqiyvddin <waqiyvddin@gmail.com>
2023-03-13 01:51:42 +0800

    bod

---
+++ resources/views/landing/ceo.blade.php
@@ -0,0 +1,168 @@
+<!-- ======= About Section ======= -->
+<section  class="about">
+    <div class="container">
+        
+        <div class="row content">
+        <div class="col-lg-4">
+            <div class="section-title" data-aos="fade-right">
+            <h2>OUR BOD</h2>
+            <p></p>
+            </div>
+        </div>
+
+        <div class="col-lg-8" >
+            <div class="ceo row">
+                <div class="gambar d-flex justify-content-center col-md">
+                    <img src="assets/img/exa-group/ceo-hafiz-halim-small.png" style="height: 444px; width: 100%; object-fit: cover ;" class="img-fluid" alt="">
+                </div>
+        
+                <div class="perincian col-md" data-aos="fade-left" data-aos-delay="200">
+                    <h4 style="">HAFIZ HALIM</h4>
+                    <h6>CEO/ Founder</h6>
+                    <br>
+                    <p>
+                    Hafiz Halim, Chief Executive Officer/ Founder is a wellknown technopreneur in the industry equipped with expertise
+                    both in technical and management. He has extensive working experience and heading many largescale projects during
+                    his tenure with Formis Network Services, Nokia Siemens Network and Sun Microsystems.
+                    </p>
+                </div>
+            </div>
+            <div class=" pt-10"></div>
+
+            <div class="ceo row">
+                <div class="gambar d-flex justify-content-center col-md">
+                    <img src="assets\img\exa-group\bod-web-6.png" style="height: 444px; width: 100%; object-fit:cover  ;" class="img-fluid" alt="">
+                </div>
+        
+                <div class="perincian col-md" data-aos="fade-left" data-aos-delay="200">
+                    <h4 style="">MARZELITA REJAB</h4>
+                    <h6>Finance Director</h6>
+                    <br>
+                    <p>
+                        Marzelita Rejab, Finance Director has 20 years working experience in IT and automotive industry,
+                        handling accounting, finance and various accounting systems.
+                    </p>
+                </div>
+            </div>
+
+        </div>
+
+        {{-- <div class="col-lg-4 d-flex justify-content-center">
+            <img src="assets/img/exa-group/ceo-hafiz-halim-small.png" style="height: 444px;" class="img-fluid" alt="">
+        </div>
+
+        <div class="col-lg-4 pt-4 pt-lg-0" data-aos="fade-left" data-aos-delay="200">
+            <div class="member-info" style="color:#ed1d29;">
+            <h4 style="font-weight:bold;">HAFIZ HALIM</h4>
+            <h6>CEO/ Founder</h6>
+            </div>
+            <br>
+            <p>
+            Hafiz Halim, Chief Executive Officer/ Founder is a wellknown technopreneur in the industry equipped with expertise
+            both in technical and management. He has extensive working experience and heading many largescale projects during
+            his tenure with Formis Network Services, Nokia Siemens Network and Sun Microsystems.
+            </p>
+        </div> --}}
+        </div>
+
+    </div>
+</section>
+<!-- End About Section -->
+
+<!-- ======= CEO Section ======= -->
+{{-- <section id="ceo" class="team">
+    <div class="container"> --}}
+    
+    {{-- <div class="row">
+        <div class="col-lg-4">
+        <div class="section-title" data-aos="fade-right">
+            <h2>OUR CEO</h2>
+            <p></p>
+        </div>
+        </div>
+
+        <div class="col-lg-4">
+        <div class="member " data-aos="zoom-in" data-aos-delay="100">
+            <div class="pic">
+            <img src="assets/img/exa-group/ceo-hafiz-halim-small.png" tyle="height: 2px;" class="img-fluid"    alt="">
+            </div>
+
+
+        </div>
+        </div>
+
+        <div class="col-lg-4 pt-4 pt-lg-0" data-aos="fade-left" data-aos-delay="200">
+        <div class="member-info">
+            <h4>HAFIZ HALIM</h4>
+            <h6>CEO/ Founder</h6>
+        </div>
+        <p>
+            Hafiz Halim, Chief Executive Officer/ Founder is a wellknown technopreneur in the industry equipped with expertise
+            both in technical and management. He has extensive working experience and heading many largescale projects during
+            his tenure with Formis Network Services, Nokia Siemens Network and Sun Microsystems.
+        </p>
+        </div>
+    </div> --}}
+
+        
+        {{-- <div class="col-lg-8">
+
+        <div class="row box-1 ">
+
+            <div class="  col-md-4 col-12">
+            <div class="member " data-aos="zoom-in" data-aos-delay="100">
+                <div class="pic">
+                <img src="assets/img/exa-group/ceo-hafiz-halim-small.png" class="img-fluid"  alt="">
+                </div>
+                <div class="member-info">
+                <h4>HAFIZ HALIM</h4>
+                <h6>CEO/ Founder</h6>
+                </div>
+            </div>
+            </div>
+
+            <div class=" col-md-4 col-8">
+            <div class=" member" data-aos="zoom-in" data-aos-delay="200">
+                <div class="pic"><img src="assets/img/exa-group/ceo-suhaimi-hassan-small.png" class="img-fluid" alt=""></div>
+                <div class="member-info">
+                <h4>MOHD SUHAIMI HASSAN</h4>
+                <h6>Managing Director</h6>
+                </div>
+            </div>
+            </div>
+            
+            <div class="col-lg-4">
+            <div class="member " data-aos="zoom-in" data-aos-delay="100">
+                <div class="pic">
+                <img src="assets/img/exa-group/ceo-hafiz-halim-small.png" class="img-fluid"    alt="">
+                </div>
+                <div class="member-info">
+                <h4>HAFIZ HALIM</h4>
+                <h6>CEO/ Founder</h6>
+                </div>
+
+            </div>
+            </div>
+            
+            <div class="col-lg-4">
+            <p>
+                Hafiz Halim, Chief Executive Officer/ Founder is a wellknown technopreneur in the industry equipped with expertise
+                both in technical and management. He has extensive working experience and heading many largescale projects during
+                his tenure with Formis Network Services, Nokia Siemens Network and Sun Microsystems.
+            </p>  
+            </div>
+
+            <div class="member col-md-4 col-12" data-aos="zoom-in" data-aos-delay="200">
+                <div class="pic"><img src="assets/img/exa-group/ceo-suhaimi-hassan-small.png" class="img-fluid" alt=""></div>
+                <div class="member-info">
+                <h4>MOHD SUHAIMI HASSAN</h4>
+                <h6>Managing Director</h6>
+                </div>
+            </div>
+        </div>
+
+        </div> --}}
+
+    {{-- </div>
+</section> --}}
+<!-- End Team Section -->
