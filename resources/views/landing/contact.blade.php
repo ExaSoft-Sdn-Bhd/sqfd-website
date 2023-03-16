7f31367614674e39af00e36fce0a68c66dd531c7
waqiyvddin <waqiyvddin@gmail.com>
2023-03-17 04:41:58 +0800

    iphone contact

--- resources/views/landing/contact.blade.php
+++ resources/views/landing/contact.blade.php
@@ -11,57 +11,122 @@
 
         <div class="col-lg-8" data-aos="fade-up" data-aos-delay="100">
             <div class="" >
-            <iframe style=" width: 100%; height: 300px;" src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d15935.917055949072!2d101.5572644!3d3.1001691!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xa9e770d04e07bea5!2sExamedia%20Solutions%20Sdn%20Bhd!5e0!3m2!1sen!2smy!4v1674766089587!5m2!1sen!2smy"  allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                <iframe style=" width: 100%; height: 300px;" src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d15935.917055949072!2d101.5572644!3d3.1001691!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xa9e770d04e07bea5!2sExamedia%20Solutions%20Sdn%20Bhd!5e0!3m2!1sen!2smy!4v1674766089587!5m2!1sen!2smy"  allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
             </div>
         
-            <div class="row box-contact ">
-            <div class="col-l box-contact-in info">
-                <i class="bi bi-geo-alt"></i>
-                <!-- <h4>Location:</h4> -->
-                <p>
-                No 8 & 10, Jalan Opera L U2/L, <br>
-                TTDI Jaya,  Seksyen U2, <br>
-                40150 Shah Alam, Selangor
-                </p>
-            </div>
+            {{-- <div class="row box-contact ">
+                <div class="col-l box-contact-in info">
+                    <i class="bi bi-geo-alt"></i>
+                    <!-- <h4>Location:</h4> -->
+                    <p>
+                    No 8 & 10, Jalan Opera L U2/L, <br>
+                    TTDI Jaya,  Seksyen U2, <br>
+                    40150 Shah Alam, Selangor
+                    </p>
+                </div>
 
-            <div class="col-lg-8 box-contact-in info">
-                <i class="bi bi-envelope"></i>
-                <!-- <h4>Email:</h4> -->
-                <p>
-                    <strong>Fax:</strong> +6 03 7832 0008 <br>
-                    <strong>Phone:</strong> +6 03 7832 0008 <br>
-                    <strong>Email:</strong> <a href="mailto:info@examedia.com.my">info@examedia.com.my</a>  <br>
-                </p>
-            </div>
+                <div class="col-lg-8 box-contact-in info">
+                    <i class="bi bi-envelope"></i>
+                    <!-- <h4>Email:</h4> -->
+                    <p>
+                        <strong>Fax:</strong> +6 03 7832 0008 <br>
+                        <strong>Phone:</strong> +6 03 7832 0008 <br>
+                        <strong>Email:</strong> <a href="mailto:info@examedia.com.my">info@examedia.com.my</a>  <br>
+                    </p>
+                </div>
 
-            </div>
+            </div> --}}
 
-            <div class="row box-contact ">
-            
-            <div class="col-lg-8 box-contact-in info ">
-                <i class="bi bi-phone"></i>
-                <!-- <h4>Call:</h4> -->
-                <p>
-                <strong>Monday-Thursday :</strong> 8.30 AM – 5.30 PM  <br>
-                <strong>Friday :</strong> 8.30 AM – 5.00 PM  <br>
-                <strong>Saturday & Sunday :</strong> Close <br>
-                <strong>Public Holiday :</strong> Close <br>
-
-                </p>
-            </div>
-
-            <div class="col-lg-8  box-contact-in info d-flex justify-content-center  align-items-center w-100">
-                <div class="social-links d-flex justify-content-center">
-                <!-- <a href="#" class="twitter"><i class="bx bxl-twitter"></i></a> -->
-                <a href="https://www.facebook.com/examediasolutions/" target="_blank" class="facebook"><i class="bx bxl-facebook"></i></a>
-                <a href="https://www.instagram.com/examediasolutions/" target="_blank" class="instagram"><i class="bx bxl-instagram"></i></a>
-                <a href="https://www.linkedin.com/company/examedia-solutions-sdn-bhd/mycompany/" target="_blank" class="linkedin"><i class="bx bxl-linkedin"></i></a>
-                <!-- <a href="#" class="google-plus"><i class="bx bxl-skype"></i></a> -->
+            <div class="row">
+                <div class="col-lg box-contact" style=" width:100%; ">
+                    <div class=" box-contact-in info">
+                        <i class="bi bi-geo-alt"></i>
+                        <!-- <h4>Location:</h4> -->
+                        <p>
+                        No 8 & 10, Jalan Opera L U2/L, <br>
+                        TTDI Jaya,  Seksyen U2, <br>
+                        40150 Shah Alam, Selangor
+                        <strong>Fax:</strong> +6 03 7832 0008 <br>
+                        <strong>Phone:</strong> +6 03 7832 0008 <br>
+                        </p>
+                    </div>
+                </div>
+                <div class="col-lg box-contact" style=" width:100%; ">
+                    <div class=" box-contact-in info">
+                        <i class="bi bi-envelope"></i>
+                        <!-- <h4>Email:</h4> -->
+                        <p>
+                            <strong>Fax:</strong> +6 03 7832 0008 <br>
+                            <strong>Phone:</strong> +6 03 7832 0008 <br>
+                            <strong>Email:</strong> <a href="mailto:info@examedia.com.my">info@examedia.com.my</a>  <br>
+                        </p>
+                    </div>
                 </div>
             </div>
 
+            {{-- <div class="row">
+                <div class="col-lg box-contact" style=" width:100%; background:#f1e;">
+                    <div class=" box-contact-in info">
+
+                    </div>
+                </div>
+                <div class="col-lg box-contact" style=" width:100%; background:#1fe;">
+                    <div class=" box-contact-in info">
+
+                    </div>
+                </div>
+            </div> --}}
+
+            <div class="row">
+                <div class="col-lg box-contact" style=" width:100%; ">
+                    <div class=" box-contact-in info">
+                        <i class="bi bi-phone"></i>
+                        <!-- <h4>Call:</h4> -->
+                        <p>
+                        <strong>Monday-Thursday :</strong> 8.30 AM – 5.30 PM  <br>
+                        <strong>Friday :</strong> 8.30 AM – 5.00 PM  <br>
+                        <strong>Saturday & Sunday :</strong> Close <br>
+                        <strong>Public Holiday :</strong> Close <br>
+
+                        </p>
+                    </div>
+                </div>
+                <div class="col-lg box-contact " style=" width:100%; ">
+                    <div class=" box-contact-in info  d-flex justify-content-center  align-items-center">
+                        <div class="social-links d-flex justify-content-center">
+                            <!-- <a href="#" class="twitter"><i class="bx bxl-twitter"></i></a> -->
+                            <a href="https://www.facebook.com/examediasolutions/" target="_blank" class="facebook"><i class="bx bxl-facebook"></i></a>
+                            <a href="https://www.instagram.com/examediasolutions/" target="_blank" class="instagram"><i class="bx bxl-instagram"></i></a>
+                            <a href="https://www.linkedin.com/company/examedia-solutions-sdn-bhd/mycompany/" target="_blank" class="linkedin"><i class="bx bxl-linkedin"></i></a>
+                            <!-- <a href="#" class="google-plus"><i class="bx bxl-skype"></i></a> -->
+                            </div>
+                    </div>
+                </div>
             </div>
+
+            {{-- <div class="row box-contact ">
+                <div class="col-md  box-contact-in info ">
+                    <i class="bi bi-phone"></i>
+                    <!-- <h4>Call:</h4> -->
+                    <p>
+                    <strong>Monday-Thursday :</strong> 8.30 AM – 5.30 PM  <br>
+                    <strong>Friday :</strong> 8.30 AM – 5.00 PM  <br>
+                    <strong>Saturday & Sunday :</strong> Close <br>
+                    <strong>Public Holiday :</strong> Close <br>
+
+                    </p>
+                </div>
+
+                <div class="col-12 col-md-6 box-contact-in info d-flex justify-content-center  align-items-center w-100">
+                    <div class="social-links d-flex justify-content-center">
+                    <!-- <a href="#" class="twitter"><i class="bx bxl-twitter"></i></a> -->
+                    <a href="https://www.facebook.com/examediasolutions/" target="_blank" class="facebook"><i class="bx bxl-facebook"></i></a>
+                    <a href="https://www.instagram.com/examediasolutions/" target="_blank" class="instagram"><i class="bx bxl-instagram"></i></a>
+                    <a href="https://www.linkedin.com/company/examedia-solutions-sdn-bhd/mycompany/" target="_blank" class="linkedin"><i class="bx bxl-linkedin"></i></a>
+                    <!-- <a href="#" class="google-plus"><i class="bx bxl-skype"></i></a> -->
+                    </div>
+                </div>
+            </div> --}}
 
             {{-- <div class="col-sm box-contact-in">
                 <div class="info">
