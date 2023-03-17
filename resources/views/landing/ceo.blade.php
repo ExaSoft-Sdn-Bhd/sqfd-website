c1ea88c242e67a6a46169af86eba2c68816d653d
waqiyvddin <waqiyvddin@gmail.com>
2023-03-17 11:02:42 +0800

    iP BOD update

--- resources/views/landing/ceo.blade.php
+++ resources/views/landing/ceo.blade.php
@@ -1,14 +1,170 @@
+{{-- <style>
+    /*--------------------------------------------------------------
+    # Team
+    --------------------------------------------------------------*/
+    .team .member {
+    margin-bottom: 20px;
+    overflow: hidden;
+    text-align: center;
+    /* border-radius: 5px; */
+    background: #fff;
+    box-shadow: 0px 2px 15px rgba(0, 0, 0, 0.1);
+    }
+
+    .team .member .member-img {
+    position: relative;
+    overflow: hidden;
+    }
+
+    .team .member .member-img img{
+    width: 100%;
+    height: 400px;
+    object-fit: cover ;
+    object-position: top;
+
+    }
+
+    .team .member .social {
+    position: absolute;
+    left: 0;
+    bottom: 0;
+    right: 0;
+    height: 100%;
+    opacity: 0;
+    transition: ease-in-out 0.3s;
+    background: var(--color-primary);
+    /* background: rgba(255, 255, 255, 0.85); */
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    }
+
+    .team .member .social a {
+    transition: color 0.3s;
+    color: #473d3a;
+    margin: 0 10px;
+    display: inline-flex;
+    align-items: center;
+    justify-content: center;
+    }
+
+    .team .member .social a:hover {
+    color: var(--color-primary);
+    }
+
+    .team .member .social i {
+    font-size: 18px;
+    line-height: 0;
+    }
+
+    .team .member .social h6 {
+    /* margin-bottom: auto; */
+    padding:  0px 20px;
+    color: #ffff;
+    /* margin: auto; */
+    }
+
+    .team .member .member-info {
+    padding: 25px 15px;
+    }
+
+    .team .member .member-info h4 {
+    font-weight: 700;
+    margin-bottom: 5px;
+    font-size: 18px;
+    color: #000000;
+    }
+
+    .team .member .member-info span {
+    display: block;
+    font-size: 13px;
+    font-weight: 700;
+    color: #000000;
+    }
+
+    .team .member .member-info p {
+    font-style: italic;
+    font-size: 14px;
+    line-height: 26px;
+    color: #000000;
+    }
+
+    .team .member:hover .social {
+    opacity: 1;
+    }
+</style> --}}
+ 
+<!-- ======= Team Section ======= -->
+{{-- <section id="team" class="team section-bg">
+    <div class="container">
+
+        <div class="row content">
+            <div class="col-lg-4">
+                <div class="section-title" data-aos="fade-right">
+                <h2>OUR BOARD</h2>
+                <p></p>
+                </div>
+            </div>
+
+            <div class="col-lg-8 row ">
+
+                <div class="col-lg-6 col-md-6  " data-aos="fade-up" data-aos-delay="200">
+                    <div class="member ">
+                        <div class="member-img">
+                        <img src="assets/img/exa-group/ceo-hafiz-halim-small.png" class="img-fluid" alt="">
+                        <div class="social">
+                           <h6>
+                            Hafiz Halim, Chief Executive Officer/ Founder is a well known technopreneur in the industry equipped with expertise
+                            both in technical and management. He has extensive working experience and heading many large scale projects during
+                            his tenure with Formis Network Services, Nokia Siemens Network and Sun Microsystems.
+                           </h6>
+                        </div>
+                        </div>
+                        <div class="member-info">
+                        <h4>HAFIZ HALIM </h4>
+                        <span>CEO/ Founder</span>
+                        </div>
+                    </div>
+                </div>
+
+                
+                <div class="col-lg-6 col-md-6 " data-aos="fade-up" data-aos-delay="300">
+                    <div class="member">
+                        <div class="member-img d-flex align-items-center">
+                        <img src="assets\img\exa-group\bod-web-6.png" class="img-fluid" alt="">
+                        <div class="social ">
+                            <h6>
+                                Marzelita Rejab, Finance Director has 20 years working experience in IT and automotive industry,
+                                handling accounting, finance and various accounting systems.
+                            </h6>
+                        </div>
+                        </div>
+                        <div class="member-info">
+                        <h4>MARZELITA REJAB </h4>
+                        <span>CFO (Chief Financial Officer) </span>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+
+    </div>
+</section> --}}
+<!-- End Team Section -->
+
+
 <!-- ======= About Section ======= -->
 <section  class="about">
     <div class="container">
         
         <div class="row content">
-        <div class="col-lg-4">
-            <div class="section-title" data-aos="fade-right">
-            <h2>OUR BOARD</h2>
-            <p></p>
+            <div class="col-lg-4">
+                <div class="section-title" data-aos="fade-right">
+                <h2>OUR BOARD</h2>
+                <p></p>
+                </div>
             </div>
-        </div>
 
         <div class="col-lg-8" >
             <div class="ceo row">
@@ -47,22 +203,6 @@
 
         </div>
 
-        {{-- <div class="col-lg-4 d-flex justify-content-center">
-            <img src="assets/img/exa-group/ceo-hafiz-halim-small.png" style="height: 444px;" class="img-fluid" alt="">
-        </div>
-
-        <div class="col-lg-4 pt-4 pt-lg-0" data-aos="fade-left" data-aos-delay="200">
-            <div class="member-info" style="color:#ed1d29;">
-            <h4 style="font-weight:bold;">HAFIZ HALIM</h4>
-            <h6>CEO/ Founder</h6>
-            </div>
-            <br>
-            <p>
-            Hafiz Halim, Chief Executive Officer/ Founder is a wellknown technopreneur in the industry equipped with expertise
-            both in technical and management. He has extensive working experience and heading many largescale projects during
-            his tenure with Formis Network Services, Nokia Siemens Network and Sun Microsystems.
-            </p>
-        </div> --}}
         </div>
 
     </div>
