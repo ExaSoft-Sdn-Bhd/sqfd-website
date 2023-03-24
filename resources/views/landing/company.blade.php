e0e859956b46e645841cd7b2d6730594cf80cefc
waqiyvddin <waqiyvddin@gmail.com>
2023-03-24 11:49:45 +0800

    update domain solution'S' link

--- resources/views/landing/company.blade.php
+++ resources/views/landing/company.blade.php
@@ -18,7 +18,7 @@
                     <div class="row">
                     <div class="col-xl-4 d-flex align-items-stretch" data-aos="zoom-in" data-aos-delay="100">
                         <div class="icon-box mt-4 mt-xl-0">
-                            <a href="https://solution.examedia.my" target="blank">
+                            <a href="https://solutions.examedia.my" target="blank">
                                 <div class="icon"><i class="bi bi-usb-plug"></i></div>
                                 <h4>EXAMEDIA SOLUTIONS<br> SDN BHD</h4>
                                 <p style="color: #333333;">Hardware, data centre & managed services.</p>
@@ -29,7 +29,7 @@
                         <div class="icon-box mt-4 mt-xl-0">
                             <a href="https://software.examedia.my" target="blank">
                                 <div class="icon"><i class="bi bi-laptop"></i></div>
-                                <h4>EXAMEDIA <br> SOFTWARE <br> SDN BHD</h4>
+                                <h4>EXAMEDIA SOFTWARE <br> SDN BHD</h4>
                                 <p style="color: #333333;">Enterprise software, custom build system & mobile application.</p>
                             </a>
                         </div>
@@ -38,7 +38,7 @@
                         <div class="icon-box mt-4 mt-xl-0">
                             <a href="https://technocraft.examedia.my" target="blank">
                                 <div class="icon"><i class="bi bi-hammer"></i></div>
-                                <h4>EXAMEDIA <br> TECHNOCRAFT <br> SDN BHD</h4>
+                                <h4>EXAMEDIA TECHNOCRAFT <br> SDN BHD</h4>
                                 <p style="color: #333333;">Construction & interior design.</p>
                             </a>
                         </div>
