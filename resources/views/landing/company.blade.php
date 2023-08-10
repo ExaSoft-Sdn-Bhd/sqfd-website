176949612c57cc94aab2215d2d488d0ceeaa0599
ExamediaSoftware <examediasoftware@gmail.com>
2023-08-10 18:10:13 +0800

    partners fix

--- resources/views/landing/company.blade.php
+++ resources/views/landing/company.blade.php
@@ -25,21 +25,62 @@
                             </div>
                             <div class="col-xl-4 d-flex align-items-stretch" data-aos="zoom-in" data-aos-delay="200">
                                 <div class="icon-box mt-4 mt-xl-0">
-                                    <a href="https://software.examedia.my" target="blank">
-                                        <div class="icon"><i class="bi bi-laptop"></i></div>
-                                        <h4>EXAMEDIA SOFTWARE <br> SDN BHD</h4>
-                                        <p style="color: #333333;">Enterprise software, custom build system & mobile
-                                            application.</p>
-                                    </a>
+                                    <div>
+                                        <img src="assets/img/sqfd/partner-3.png" alt="" style="width: 200px">
+                                    </div>
                                 </div>
                             </div>
                             <div class="col-xl-4 d-flex align-items-stretch" data-aos="zoom-in" data-aos-delay="300">
                                 <div class="icon-box mt-4 mt-xl-0">
-                                    <a href="https://technocraft.examedia.my" target="blank">
-                                        <div class="icon"><i class="bi bi-hammer"></i></div>
-                                        <h4>EXAMEDIA TECHNOCRAFT <br> SDN BHD</h4>
-                                        <p style="color: #333333;">Construction & interior design.</p>
-                                    </a>
+                                    <div>
+                                        <img src="assets/img/sqfd/partner-4.png" alt="" style="width: 200px">
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-xl-4 d-flex align-items-stretch" data-aos="zoom-in" data-aos-delay="100">
+                                <div class="icon-box mt-4 mt-xl-0">
+                                    <div>
+                                        <img src="assets/img/sqfd/partner-5.png" alt="" style="width: 200px">
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-xl-4 d-flex align-items-stretch" data-aos="zoom-in" data-aos-delay="200">
+                                <div class="icon-box mt-4 mt-xl-0">
+                                    <div>
+                                        <img src="assets/img/sqfd/partner-6.png" alt="" style="width: 200px">
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-xl-4 d-flex align-items-stretch" data-aos="zoom-in" data-aos-delay="300">
+                                <div class="icon-box mt-4 mt-xl-0">
+                                    <div>
+                                        <img src="assets/img/sqfd/partner-7.png" alt="" style="width: 200px">
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-xl-4 d-flex align-items-stretch" data-aos="zoom-in" data-aos-delay="100">
+                                <div class="icon-box mt-4 mt-xl-0">
+                                    <div>
+                                        <img src="assets/img/sqfd/partner-8.png" alt="" style="width: 200px">
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-xl-4 d-flex align-items-stretch" data-aos="zoom-in" data-aos-delay="200">
+                                <div class="icon-box mt-4 mt-xl-0">
+                                    <div>
+                                        <img src="assets/img/sqfd/partner-9.png" alt="" style="width: 200px">
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-xl-4 d-flex align-items-stretch" data-aos="zoom-in" data-aos-delay="300">
+                                <div class="icon-box mt-4 mt-xl-0">
+                                    <div>
+                                        <img src="assets/img/sqfd/partner-10.png" alt="" style="width: 200px">
+                                    </div>
                                 </div>
                             </div>
                         </div>
